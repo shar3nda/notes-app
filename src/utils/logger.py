a3d5601346fda038a526a1f6619e210188832e67
Ivan Krupnov <ikrupnov@progmaticlab.com>
2025-04-26 09:25:50 +0300

    simplify logging

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,23 +1,11 @@
-from datetime import datetime, timezone
+from datetime import datetime
 import logging
 
 
-class LogfmtFormatter(logging.Formatter):
-    def __get_log_string(self, log_record: dict) -> dict:
-        parts = []
-        for key, value in log_record.items():
-            if isinstance(value, str) and (" " in value or "=" in value):
-                value = f'"{value}"'
-            parts.append(f"{key}={value}")
-        return " ".join(parts)
-
-    def format(self, record):
-        log_record = {
-            "level": record.levelname.lower(),
-            "time": datetime.fromtimestamp(record.created, timezone.utc).isoformat(),
-            "module": record.name,
-            "line": record.lineno,
-            "correlation_id": getattr(record, "correlation_id", "-"),
-            "message": record.getMessage().replace('"', '\\"'),
-        }
-        return self.__get_log_string(log_record)
+class LogFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        return (
+            datetime.fromtimestamp(record.created)
+            .astimezone()
+            .isoformat(timespec="milliseconds")
+        )
